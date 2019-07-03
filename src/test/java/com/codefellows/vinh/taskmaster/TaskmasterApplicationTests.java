474b05c97d60aca0bf10fb9b2508d4b934b258d2
Vinh Nguyen <nguyenv2@outlook.com>
2019-07-03 09:21:24 -0700

    responsebody

--- src/test/java/com/codefellows/vinh/taskmaster/TaskmasterApplicationTests.java
+++ src/test/java/com/codefellows/vinh/taskmaster/TaskmasterApplicationTests.java
@@ -1,16 +1,31 @@
 package com.codefellows.vinh.taskmaster;
 
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.codefellows.vinh.taskmaster.repository.TaskRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = TaskmasterApplication.class)
+@WebAppConfiguration
+@ActiveProfiles("local")
 public class TaskmasterApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+	private DynamoDBMapper dynamoDBMapper;
+
+	@Autowired
+	private AmazonDynamoDB amazonDynamoDB;
+
+	@Autowired
+	TaskRepository taskRepository;
+
+
 
 }
